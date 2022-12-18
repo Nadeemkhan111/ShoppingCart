33403932ab87cf6ba71c6d219296ea287a2c83f0
Nadeemkhan111 <pathannadeem4786@gmail.com>
2022-12-19 00:56:57 +0530

    new shopping project

---
+++ src/Components/Back/Data/Data.jsx
@@ -0,0 +1,59 @@
+const data = {
+  productItem: [
+    {
+      id: "1",
+      name: "Beats Headphone",
+      price: 999,
+      img: "",
+    },
+    {
+      id: "2",
+      name: "Sol Headphone",
+      price: 899,
+      img: "",
+    },
+    {
+      id: "3",
+      name: "Sennheiser",
+      price: 799,
+      img: "",
+    },
+    {
+      id: "4",
+      name: "Iphone X",
+      price: 1199,
+      img: "",
+    },
+    {
+      id: "5",
+      name: "Samsung",
+      price: 1099,
+      img: "",
+    },
+    {
+      id: "6",
+      name: "OnePlus",
+      price: 1299,
+      img: "",
+    },
+    {
+      id: "7",
+      name: "Macbook",
+      price: 2299,
+      img: "",
+    },
+    {
+      id: "8",
+      name: "Acer laptop",
+      price: 1599,
+      img: "",
+    },
+    {
+      id: "9",
+      name: "Asus laptop",
+      price: 1699,
+      img: "",
+    },
+  ],
+};
+export default data;
